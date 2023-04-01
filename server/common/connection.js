d832faa1f6fd83022f0c1b59eb1da7c95596e098
unknown <efpa1998@hotmail.com>
2023-04-01 12:37:24 -0500

    .env file created

--- server/common/connection.js
+++ server/common/connection.js
@@ -1,11 +1,12 @@
 const { Client } = require('pg')
+require('dotenv').config()
 
 const db = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'course',
-  password: '1234',
-  port: 5432, 
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
 module.exports = db
